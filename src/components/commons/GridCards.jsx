260e289c79f041effa4a78e0923fe6628c5ecac6
hj <jjjjhjjjj123@gmail.com>
2020-09-13 23:18:33 +0900

    modify GridCartds Link

--- src/components/commons/GridCards.jsx
+++ src/components/commons/GridCards.jsx
@@ -6,7 +6,7 @@ const GridCards = ({ movieId, image, movieName, landingPage, characterName }) =>
 		return (
 			<Col lg={6} md={8} xs={24}>
 				<div style={{ position: 'relative' }}>
-					<a href={`/Movie-App/movie/${movieId}`}>
+					<a href={`/movie/${movieId}`}>
 						<img src={image} alt={movieName} style={{ width: '100%', height: '320px' }} />
 					</a>
 				</div>
