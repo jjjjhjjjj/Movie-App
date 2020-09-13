9086224af3bafdcc79274d65be76aa383d510a08
hj <jjjjhjjjj123@gmail.com>
2020-09-14 00:17:30 +0900

    modify GridCartds Link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 	return (
 		<>
 			<Switch>
-				<Route path="/Movie-App/movie/:movieId" component={MovieDetail} />
+				<Route path="/movie/:movieId" component={MovieDetail} />
 				<Route path="/" exact component={LandingPage} />
 			</Switch>
 		</>
